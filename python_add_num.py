3993275628f906497bd6ebcd0d4e30d92e09fbf2
anu0o7 <anirudhpeddi007@gmail.com>
2024-01-11 16:16:54 +0530

    Editied pyaddnum to close the brackets

--- python_add_num.py
+++ python_add_num.py
@@ -1,7 +1,6 @@
 def add(x,y):
    print(x+y)
-
-x = int(input(" Enter a Number: " )
-y = int(input("Enter another number: ")
-
-num = add(x,y)
+   
+x=int(input("Enter a number "))
+y=int(input("Enter another number "))
+num=add(x,y)
